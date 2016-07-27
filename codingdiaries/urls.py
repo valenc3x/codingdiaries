58a0d559bd7ef15d3b0e714643efa23ca31162a9
valenc3x <ricardo.ivan91@gmail.com>
2016-07-27 04:52:32 -0500

    new base templates added

--- codingdiaries/urls.py
+++ codingdiaries/urls.py
@@ -3,7 +3,9 @@ from django.contrib import admin
 
 urlpatterns = [
     # Base urls
-    url(r'^$', 'codingdiaries.views.home', name='home'),
+    url(r'^$', 'codingdiaries.views.landing', name='landing'),
+    url(r'^home$', 'codingdiaries.views.home', name='home'),
+    url(r'^login$', 'codingdiaries.views.login' , name='login'),
     # Admin site
     url(r'^grappelli/', include('grappelli.urls')),
     url(r'^admin/', include(admin.site.urls)),
--- codingdiaries/views.py
+++ codingdiaries/views.py
@@ -1,9 +1,26 @@
+from django.contrib.auth import authenticate, login
 from django.shortcuts import render
+
+def landing(request):
+    template_vars = {}
+    return render(
+        request,
+        'landing.html',
+        context=template_vars
+    )
 
 def home(request):
     template_vars = {}
     return render(
         request,
         'home.html',
+        context=template_vars
+    )
+
+def login(request):
+    template_vars = {}
+    return render(
+        request,
+        'login.html',
         context=template_vars
     )
